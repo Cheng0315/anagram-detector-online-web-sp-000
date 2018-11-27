ded9d2c0e398e96fe145fab18f769b83391c28c8
Cheng Vue <cvu3@ucsc.edu>
2018-11-27 16:07:14 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def intialize(word)
+    @anagram = word
+  end
+
+  def match(arr)
+    arr.select do |word|
+      if word.char.sort == @anagram.char.sort
+        word
+      end
+    end
+  end
+end
